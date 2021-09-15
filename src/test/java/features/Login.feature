93ed493b2ee0d7e732229d3b351bea2b614bf02d
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-09-15 12:25:29 +0530

    Code modified by implementing regex patterns

--- src/test/java/features/Login.feature
+++ src/test/java/features/Login.feature
@@ -2,6 +2,12 @@ Feature: Application Login
 
   Scenario: Home page default login
     Given user is on NetBanking landing page
-    When user login into application with username and password
+    When user login into application with "jin" and "1234"
     Then home page is populated
-    And all the cards are displayed
+    And all displayed cards are "true"
+
+  Scenario: Home page default login
+    Given user is on NetBanking landing page
+    When user login into application with "jhon" and "4321"
+    Then home page is populated
+    And all displayed cards are "false"
--- src/test/java/stepDefinitions/stepDefinition.java
+++ src/test/java/stepDefinitions/stepDefinition.java
@@ -10,15 +10,17 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 public class stepDefinition {
     @Given("^user is on NetBanking landing page$")
-    public void user_is_on_netbanking_landing_page() throws Throwable {
+    public void user_is_on_netBanking_landing_page() throws Throwable {
         //code to land on homepage
         System.out.println("landed on homepage");
     }
 
-    @When("^user login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() throws Throwable {
-        //code to login with username and password
-        System.out.println("login success");
+    @When("^user login into application with \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void user_login_into_application_with_something_and_something(String strArg1, String strArg2) throws Throwable {
+        //code to login to the page
+        System.out.println(strArg1);
+        System.out.println(strArg2);
+        System.out.println("login successful");
     }
 
     @Then("^home page is populated$")
@@ -27,9 +29,10 @@ public class stepDefinition {
         System.out.println("homepage validated");
     }
 
-    @And("^all the cards are displayed$")
-    public void all_the_cards_are_displayed() throws Throwable {
+    @And("^all displayed cards are \"([^\"]*)\"$")
+    public void all_displayed_cards_are_something(String strArg1) throws Throwable {
         //validating cards
+        System.out.println(strArg1);
         System.out.println("cards validated");
     }
 }
